78eae17e3c26a3f2df0d85335d85a5261ba851a8
oliseh6123 <oliseh.obiajuru@gmail.com>
2021-02-27 09:36:37 +0100

    Home screen product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 
 const App = () => {
   return (
     <>
-    <h1>Welcome to Proshop</h1>
+    <Header/>
+    <Container>
+    <main className='py-3'>
+    <HomeScreen/>
+    </main>
+    </Container>
+    <Footer/>
     </>
   );
 }
